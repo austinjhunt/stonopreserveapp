dc8bd80a41dacaa120d1acdb40803b9ddbd01132
Daniel <danielaustinlee@gmail.com>
2019-02-12 22:36:06 -0500

    Add user and lock code models

--- srpaccessmgmt/srpwebapp/models.py
+++ srpaccessmgmt/srpwebapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_email = models.CharField(max_length=50)
+
+
+class LockCode(models.Model):
+    current_code = models.CharField(max_length=4)
+    
